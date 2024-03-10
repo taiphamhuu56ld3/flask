65b7a208cfaa66dbac3b646cde0981f24989f8eb
TaiPham <115418952+taiphamhuu56ld3@users.noreply.github.com>
2024-03-10 17:12:51 +0700

    [FEAT] FA1: Register blue print

--- RESTfull_API/library/__init__.py
+++ RESTfull_API/library/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, Blueprint
+from .book.controller import books
 
 def create_app(config_file="config.py"):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
+    app.register_blueprint(books)
     return app
