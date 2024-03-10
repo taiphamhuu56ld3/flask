f3b2fc165566ac0aba320823933cd685fc367d65
TaiPham <115418952+taiphamhuu56ld3@users.noreply.github.com>
2024-03-10 16:50:24 +0700

    [FEAT] FA1: Set config from config file

--- RESTfull_API/library/__init__.py
+++ RESTfull_API/library/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Blueprint
 
-def create_app():
+def create_app(config_file="config.py"):
     app = Flask(__name__)
+    app.config.from_pyfile(config_file)
     return app
