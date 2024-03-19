895ee220d42ae8fb968d8b1d64b0e483af008853
taipham <taipham56ld3@gmail.com>
2024-03-20 00:04:39 +0700

    [FIX] FA2: FIX error when get the db

--- RESTfull_API/library/__init__.py
+++ RESTfull_API/library/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, Blueprint
 from .book.controller import books
-
+from .model import Students, Books, Author, Category, Borrows
 from .extension import db
+
+import os
+def create_db(app):
+    with app.app_context():
+        if not os.path.exists("library/library.db"):
+            db.create_all()
+            print("Created DB!")
 
 def create_app(config_file="config.py"):
     app = Flask(__name__)
-    
-    db.init_app(app)
-
     app.config.from_pyfile(config_file)
+    db.init_app(app)
+    create_db(app)
     app.register_blueprint(books)
     return app
